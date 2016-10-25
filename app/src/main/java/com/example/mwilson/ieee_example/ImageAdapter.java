05415a3524db6dbb1a8d82ac78e2261729834451
Maddie Wilson <mwilson@wirelessprv-10-192-148-45.near.illinois.edu>
2016-10-24 20:48:03 -0500

    Initial commit

---
+++ app/src/main/java/com/example/mwilson/ieee_example/ImageAdapter.java
@@ -0,0 +1,113 @@
+package com.example.mwilson.ieee_example;
+
+import android.content.Context;
+import android.view.Display;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridLayout;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by mwilson on 10/21/16.
+ */
+
+public class ImageAdapter extends BaseAdapter {
+    private Context mContext;
+
+    public ImageAdapter(Context c) {
+        mContext = c;
+    }
+
+    public int getCount() {
+        return mThumbIds.length;
+    }
+
+    public Object getItem(int position) {
+        return null;
+    }
+
+    public long getItemId(int position) {
+        return mThumbIds[position];
+    }
+
+    // create a new ImageView for each item referenced by the Adapter
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView;
+        if (convertView == null) {
+            // if it's not recycled, initialize some attributes
+            imageView = new ImageView(mContext);
+            //Display display = parent.getDisplay();
+            //GridLayout.LayoutParams params = (GridLayout.LayoutParams) parent.getLayoutParams();
+            //params.width = (parent.getWidth()/4 - params.rightMargin);
+            imageView.setLayoutParams(new GridView.LayoutParams(120, parent.getMeasuredWidth()/4));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageView.setPadding(1, 1, 1, 1);
+        } else {
+            imageView = (ImageView) convertView;
+        }
+
+        imageView.setImageResource(mThumbIds[position]);
+        return imageView;
+    }
+
+    public void switchViews(int oldPosition, int newPosition) {
+        mThumbIds[newPosition] = mThumbIds[oldPosition];
+        mThumbIds[oldPosition] = 0;
+    }
+
+    public void shufflePuzzle() {
+
+        List<Integer> itemsToShuffle = new ArrayList<>();
+        for (int i = 0; i < mThumbIds.length; i++) {
+            itemsToShuffle.add(mThumbIds[i]);
+        }
+
+        Collections.shuffle(itemsToShuffle);
+
+        for (int i = 0; i < itemsToShuffle.size(); i++) {
+            mThumbIds[i] = itemsToShuffle.get(i);
+        }
+    }
+
+    public boolean isWin() {
+
+        if (mThumbIds[0] == R.drawable.num1 &&
+                mThumbIds[1] == R.drawable.num2 &&
+                mThumbIds[2] == R.drawable.num3 &&
+                mThumbIds[3] == R.drawable.num4 &&
+                mThumbIds[4] == R.drawable.num5 &&
+                mThumbIds[5] == R.drawable.num6 &&
+                mThumbIds[6] == R.drawable.num7 &&
+                mThumbIds[7] == R.drawable.num8 &&
+                mThumbIds[8] == R.drawable.num9 &&
+                mThumbIds[9] == R.drawable.num10 &&
+                mThumbIds[10] == R.drawable.num11 &&
+                mThumbIds[11] == R.drawable.num12 &&
+                mThumbIds[12] == R.drawable.num13 &&
+                mThumbIds[13] == R.drawable.num14 &&
+                mThumbIds[14] == R.drawable.num15 &&
+                mThumbIds[15] == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    // references to our images
+    private Integer[] mThumbIds = {
+            R.drawable.num1, R.drawable.num2,
+            R.drawable.num3, R.drawable.num4,
+            R.drawable.num5, R.drawable.num6,
+            R.drawable.num7,
+            R.drawable.num8, R.drawable.num9,
+            R.drawable.num10, R.drawable.num11,
+            R.drawable.num12, R.drawable.num13,
+            R.drawable.num14, R.drawable.num15,
+            0
+    };
+}
---
+++ app/src/main/java/com/example/mwilson/ieee_example/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.mwilson.ieee_example;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.GridView;
+import android.widget.Toast;
+
+import java.util.Collections;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final GridView gridView = (GridView) findViewById(R.id.gridview);
+        gridView.setAdapter(new ImageAdapter(this));
+
+        //instantiate button
+        Button resetButton = (Button) findViewById(R.id.reset_button);
+        resetButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ImageAdapter imageAdapter = (ImageAdapter) gridView.getAdapter();
+                imageAdapter.shufflePuzzle();
+                imageAdapter.notifyDataSetChanged();
+            }
+        });
+
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                
+                ImageAdapter imageAdapter = (ImageAdapter) gridView.getAdapter();
+
+                Log.i("Id", "" + imageAdapter.getItemId(position));
+
+                //if id above is empty, shift element up
+                if (position > 3 && gridView.getAdapter().getItemId(position - 4) == 0) {
+                    imageAdapter.switchViews(position, position - 4);
+                    imageAdapter.notifyDataSetChanged();
+
+                }
+                //shift down
+                else if (position < 12 && gridView.getAdapter().getItemId(position + 4) == 0) {
+                    imageAdapter.switchViews(position, position + 4);
+                    imageAdapter.notifyDataSetChanged();
+
+                }
+                //shift left
+                else if (position % 4 != 0 && gridView.getAdapter().getItemId(position -1) == 0) {
+                    imageAdapter.switchViews(position, position - 1);
+                    imageAdapter.notifyDataSetChanged();
+
+                }
+                //shift right
+                else if (position % 4 != 3 && gridView.getAdapter().getItemId(position + 1) == 0) {
+                    imageAdapter.switchViews(position, position + 1);
+                    imageAdapter.notifyDataSetChanged();
+                }
+
+
+                //check for win
+                if (imageAdapter.isWin()) {
+                    Toast.makeText(MainActivity.this, "Congratulations, you won!   ", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+
+    }
+}
